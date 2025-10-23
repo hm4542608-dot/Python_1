d03165fab809f2e9d50ff4cba9e6742054f1828a
hm4542608-dot <hm4542608@gmail.com>
2025-10-22 22:32:52 -0700

    Add files via upload

---
+++ python_whitespace_tabs_new lines.py
@@ -0,0 +1,19 @@
+#Adding whitespace in strings
+#1.adding a tab(\t)
+student1_name = "Hassaan Mahmood"
+student2_name = "Hayyaan Mahmood"
+student3_name = "Habbaan Mahmood"
+print(student1_name + "\t" + student2_name + "\t" + student3_name )
+#2.adding a newline(\n)
+my_name = "Hassaan Mahmood"
+print(my_name + "\nLahore, Pakistan")
+#3.combining tabs and newlines
+teacher1 = "Sir Mahmood"
+teacher2 = "Sir Kasif"
+teacher3 = "Sir Ahmad"
+teacher4 = "Sir Hassan"
+print("\nteachers list:")
+print("\t" + teacher1)
+print("\t" + teacher2)
+print("\t" + teacher3)
+print("\t" + teacher4)
